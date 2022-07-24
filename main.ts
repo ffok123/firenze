2cb83dab026875f93852bbb8cd58a281cfe2d836
ffok123 <30338770+ffok123@users.noreply.github.com>
2022-07-24 17:31:13 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-basic.forever(function () {
+let item = 0
+input.onButtonPressed(Button.A, function () {
     basic.showString("MaMa")
     basic.showLeds(`
         . # . # .
@@ -11,4 +12,37 @@ basic.forever(function () {
     basic.showIcon(IconNames.SmallHeart)
     basic.pause(100)
     basic.showString("Hailey")
+})
+input.onGesture(Gesture.Shake, function () {
+    item = randint(0, 3)
+    if (item == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (item == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    }
+    if (item == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+	
 })
